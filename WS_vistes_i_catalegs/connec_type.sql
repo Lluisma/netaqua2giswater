41be43b4eef8fd944f0126061bb77f145e90547a
Lluisma <llmarti@aiguesmataro.cat>
2018-07-03 13:04:26 +0200

    CONNEC_TYPE : Adds description

--- WS_vistes_i_catalegs/connec_type.sql
+++ WS_vistes_i_catalegs/connec_type.sql
@@ -1,26 +1,26 @@
 
 CREATE OR REPLACE VIEW CONNEC_TYPE AS 
 
-	SELECT  'FONT_ORN'	    id,
-		    	'FOUNTAIN'	    type,
-			    'man_fountain'	man_table,
-			    'true'			active,
-			    'true'		    code_autofill,
-			    null		    descript,
-			    null		    link_path
+	SELECT  'FONT_ORN'	            AS "id",
+		    	'FOUNTAIN'	            AS "type",
+			    'man_fountain'	        AS man_table,
+			    'true'			            AS active,
+			    'true'		              AS code_autofill,
+			    'Font Ornamental'		    AS descript,
+			    null		                AS link_path
 	FROM DUAL
 	UNION
-	SELECT	'BOCA_REG',  'GREENTAP', 'man_greentap', 'true', 'true',  null, null
+	SELECT	'BOCA_REG',  'GREENTAP', 'man_greentap', 'true', 'true',  'Boca de reg',     null
 	FROM DUAL
 	UNION
-	SELECT	'PUNT_CAR',  'GREENTAP', 'man_greentap', 'true', 'true',  null, null
+	SELECT	'PUNT_CAR',  'GREENTAP', 'man_greentap', 'true', 'true',  'Punt de càrrega', null
 	FROM DUAL
 	UNION
-	SELECT	'FONT',      'TAP',      'man_tap',      'true', 'false', null, null
+	SELECT	'FONT',      'TAP',      'man_tap',      'true', 'false', 'Font',            null
 	FROM DUAL
   UNION
-	SELECT	'ABEURADOR', 'TAP',      'man_tap',      'true', 'false', null, null
+	SELECT	'ABEURADOR', 'TAP',      'man_tap',      'true', 'false', 'Abeurador',       null
 	FROM DUAL
 	UNION
-	SELECT	'ESCOMESA',  'WJOIN',    'man_wjoinp',   'true', 'false', null, null
+	SELECT	'ESCOMESA',  'WJOIN',    'man_wjoinp',   'true', 'false', 'Escomesa',        null
 	FROM DUAL;
