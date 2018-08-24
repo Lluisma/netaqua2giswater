e7f3d4d450781560b16a38fb42eed3359dae8d1f
Lluisma <lluis@manies.cat>
2018-08-24 12:54:32 +0200

    no message

--- UD_vistes_i_catalegs/plan_psector.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE OR REPLACE VIEW cat_mat_gully AS
-
-  SELECT ID_CAIXO                     AS "id",
-         INITCAP( CAIXO )             AS descript,
-         null                         AS "link"
-  FROM   NS_MATARO.CAT_EMBORNAL_CAIXO;
