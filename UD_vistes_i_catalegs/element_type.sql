da058d99b8e3e090408e17f261724f93ac601438
Lluisma <lluis@manies.cat>
2019-01-10 08:31:10 +0100

    Fills descript field

--- UD_vistes_i_catalegs/element_type.sql
+++ UD_vistes_i_catalegs/element_type.sql
@@ -4,9 +4,9 @@ CREATE OR REPLACE VIEW ELEMENT_TYPE AS
 	SELECT  'TAPA'			    AS "id",
         	'true'				  AS active,
           'true'		      AS code_autofill,
-          null		        AS descript,
+          'Tapa'		        AS descript,
           null		        AS link_path
 	FROM DUAL
 	UNION
-	SELECT	'GRAO', 'true', 'true', null, null
+	SELECT	'GRAO', 'true', 'true', 'Graó', null
 	FROM DUAL;
--- WS_vistes_i_catalegs/element_type.sql
+++ WS_vistes_i_catalegs/element_type.sql
@@ -4,15 +4,15 @@ CREATE OR REPLACE VIEW ELEMENT_TYPE AS
 	SELECT  'AIXETA'	    id,
           'true'			  active,
           'true'		    code_autofill,
-          null		      descript,
+          'Aixeta'      descript,
           null		      link_path
 	FROM DUAL
 	UNION
-	SELECT	'ARQFONT','true','true',null,null
+	SELECT	'ARQFONT','true','true','Arqueta Font',null
 	FROM DUAL
 	UNION
-	SELECT	'ARQUETA','true','true',null,null
+	SELECT	'ARQUETA','true','true','Arqueta',null
 	FROM DUAL
 	UNION
-	SELECT	'PORTELLA','true','true',null,null
+	SELECT	'PORTELLA','true','true','Portella',null
 	FROM DUAL;
